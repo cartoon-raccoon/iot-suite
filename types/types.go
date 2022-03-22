9090de29296ddf9461ccf1db7fdaa9053fdafecc
cartoon-raccoon <shaughn.chan@gmail.com>
2022-03-23 01:41:30 +0800

    Added new methods for sample

--- types/types.go
+++ types/types.go
@@ -1,17 +1,39 @@
 package types
 
-import "path"
+import (
+	"os"
+	"path"
+	"path/filepath"
+)
 
 // A Sample represents a malware sample to be tested.
 type Sample struct {
 	Name string
 	Path string
+	Data []byte
 }
 
 // NewSample constructs a new Sample.
-func NewSample(filepath string) *Sample {
-	return &Sample{
-		Name: path.Base(filepath),
-		Path: filepath,
+func NewSample(fpath string) (*Sample, error) {
+	abs, err := filepath.Abs(fpath)
+	if err != nil {
+		return nil, err
 	}
+	return &Sample{
+		Name: path.Base(fpath),
+		Path: abs,
+	}, nil
+}
+
+// ReadIn reads the data from the file into an in-memory buffer.
+func (sample *Sample) ReadIn() error {
+	dat, err := os.ReadFile(sample.Path)
+
+	if err != nil {
+		return err
+	}
+
+	sample.Data = dat
+
+	return nil
 }
