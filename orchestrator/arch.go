80f9ded19cbff2d5d009393caa717865a176b38f
cartoon-raccoon <shaughn.chan@gmail.com>
2022-03-23 15:11:04 +0800

    Added additional orchestrator functions

---
+++ orchestrator/arch.go
@@ -0,0 +1,20 @@
+package orchestrator
+
+const (
+	// ARM architecture
+	ARCH_ARM = iota
+	// MIPS architecture
+	ARCH_MIPS
+	// MIPSEL architecture
+	ARCH_MIPSEL
+	// Motorola 68K architecture
+	ARCH_M68K
+	// PowerPC Architecture
+	ARCH_PPC
+	// Intel i386 architecture
+	ARCH_I386
+	// AMD64 architecture
+	ARCH_AMD64
+	// Architecture unknown/not yet known
+	ARCH_UNK
+)
--- orchestrator/orchestrator.go
+++ orchestrator/orchestrator.go
@@ -1,4 +1,42 @@
 package orchestrator
 
+import (
+	"errors"
+	"fmt"
+	"os/exec"
+)
+
 type Orchestrator struct {
 }
+
+// CheckPathExes checks that all required executables are present
+func CheckPathExes() error {
+	required := []string{
+		QEMU_ARM_CMD, QEMU_MIPS_CMD,
+		QEMU_MIPSEL_CMD, QEMU_M68K_CMD,
+		QEMU_PPC_CMD, QEMU_I386_CMD,
+		QEMU_AMD64_CMD, STRINGS_CMD,
+		SSDEEP_CMD,
+	}
+
+	for _, elem := range required {
+		_, err := exec.LookPath(elem)
+		if err != nil {
+			return errors.New(fmt.Sprintf("%s not in $PATH", elem))
+		}
+	}
+
+	return nil
+}
+
+const (
+	QEMU_ARM_CMD    = "qemu-system-arm"
+	QEMU_MIPS_CMD   = "qemu-system-mips"
+	QEMU_MIPSEL_CMD = "qemu-system-mipsel"
+	QEMU_M68K_CMD   = "qemu-system-m68k"
+	QEMU_PPC_CMD    = "qemu-system-ppc"
+	QEMU_I386_CMD   = "qemu-system-i386"
+	QEMU_AMD64_CMD  = "qemu-system-x86_64"
+	STRINGS_CMD     = "strings"
+	SSDEEP_CMD      = "ssdeep"
+)
