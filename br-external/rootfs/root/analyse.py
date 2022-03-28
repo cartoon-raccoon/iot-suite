b2e8089fd7c63a89e0fc2817f47ccdb6a4ada960
cartoon-raccoon <shaughn.chan@gmail.com>
2022-03-28 09:30:39 +0800

    worked on analyse script

--- br-external/rootfs/root/analyse.py
+++ br-external/rootfs/root/analyse.py
@@ -3,9 +3,43 @@
 #!!!!! DO NOT RUN THIS ON THE HOST MACHINE
 
 import subprocess
+import sys
+import logging
+
+MINUTES_TO_RUN = 1
+SECONDS_TO_RUN = MINUTES_TO_RUN * 60
+
+processes = dict()
 
 def main():
-    pass
+    # spawn inetsim and add it to the process management dict
+    #! if we spawn inetsim as a system service, remove this line
+    inetsim = subprocess.Popen(["/usr/bin/inetsim"])
+    processes["inetsim"] = inetsim
+
+    # get the file to analyse and restrict filename to 8 characters
+    f = sys.argv[1]
+
+    usef = f[:8] if len(f) > 8 else f
+
+    # run dumpcap
+    dumpcap_args = [
+        "/usr/bin/dumpcap",
+        "-a", "duration:60",
+        "-w", f"{usef}.pcapng",
+    ]
+    dumpcap = subprocess.Popen(dumpcap_args)
+    processes["dumpcap"] = dumpcap
+
+    # construct strace args
+    strace_args = [
+        "/usr/bin/strace",
+        # output to file strace_<truncated_file>.<pid>
+        "-o", f"strace_{usef}",
+        "-ff", f
+    ]
+    strace = subprocess.Popen(strace_args)
+    processes["strace"] = strace
 
 if __name__ == "__main__":
     main()
