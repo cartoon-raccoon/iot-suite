50411aaa25a1de3fefdf714af64623b3f7633302
cartoon-raccoon <shaughn.chan@gmail.com>
2022-04-18 17:44:42 +0800

    Added additional options for starting cnc

--- scripts/start-cnc.sh
+++ scripts/start-cnc.sh
@@ -2,6 +2,19 @@
 
 VM_FOLDER="vms"
 
+if [[ "$1" == "-h" ]]; then
+    FAKE_NET_ARGS="-nic tap,model=virtio-net-pci,helper=/usr/lib/qemu/qemu-bridge-helper,mac=52:54:02:12:34:56"
+    echo "[*] Starting fakec2 on host-only network"
+    echo "NOTE: There is no internet connection in this config"
+elif [[ "$1" == "-i" ]]; then
+    FAKE_NET_ARGS=""
+    echo "[*] Starting fakec2 with internet connection"
+    echo "NOTE: The fakec2 is unable to communicate with the test VMs in this config"
+else
+    echo "[!] No valid option given, exiting"
+    exit 1
+fi
+
 if [[ $(basename $(pwd)) != "iot-suite" ]]; then
     echo "ERROR: run from project root"
     exit 1
@@ -13,5 +26,4 @@ qemu-system-x86_64 \
     -drive file=rootfs.qcow2,format=qcow2 \
     -enable-kvm \
     -m 2G \
-    -smp 2 \
-    -nic tap,model=virtio-net-pci,helper=/usr/lib/qemu/qemu-bridge-helper,mac=52:54:02:12:34:56
+    -smp 2 ${FAKE_NET_ARGS}
