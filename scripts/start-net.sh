ad0c8e95d2527ef0008ca58c2b54de41c1848f68
cartoon-raccoon <shaughn.chan@gmail.com>
2022-06-10 10:20:15 +0800

    added allowed ports

--- scripts/start-net.sh
+++ scripts/start-net.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+declare -Ar ports=(21 22 23 25 443 990 1 995 37 7 9 6667 13 2222 80 2224)
 
 # config
 bridge_name="br0"
-ip_addr="192.168.0.1/24"
+ip_addr="192.168.0.1"
+netmask="24"
 cnc_ip="192.168.0.2"
 
 # set up bridge device with ip, start dhcp, add iptables rules
@@ -15,12 +17,12 @@ setup() {
     ip link set "$bridge_name" up
 
     echo "[*] Assigning IP address $ip_addr to bridge '$bridge_name'"
-    ip addr add "$ip_addr" brd + dev "$bridge_name"
+    ip addr add "$ip_addr/$netmask" brd + dev "$bridge_name"
 
     echo "[*] Starting dhcpd"
     dhcpd
 
-    natsetup
+    # natsetup
 }
 
 # teardown all devices, kill dhcpd, remove iptables rules
@@ -33,8 +35,8 @@ teardown() {
     ip link delete "$bridge_name" type bridge
 
     echo "[*] Deleting iptables rules"
-    iptables -t nat -D PREROUTING 1
-    iptables -t nat -D POSTROUTING 1
+    iptables -t nat -F
+    iptables -t mangle -F
 }
 
 # restart dhcpd to apply new settings
@@ -49,12 +51,16 @@ dhcprestart() {
 # apply iptables rules
 natsetup() {
     echo "[*] Adding iptables rules using bridge '$bridge_name' and CNC IP '$cnc_ip'"
+
+    for port in "${ports[@]}"; do
+        echo "Adding iptables for port $port"
+        iptables -t nat -A PREROUTING -i $bridge_name -p tcp --dport $port \
+            -s 192.168.0.3 \
+            -j DNAT --to-destination $cnc_ip:$port
+    done
+
     iptables -t nat \
-        -A PREROUTING -i $bridge_name \
-        -j DNAT --to-destination $cnc_ip
-    iptables -t nat \
-        -I POSTROUTING -s $cnc_ip -o $bridge_name \
-        -j MASQUERADE
+        -I POSTROUTING -o $bridge_name -j MASQUERADE
     
     echo "Current iptables rules setup:"
     iptables -t nat -L
