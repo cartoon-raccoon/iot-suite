b753b3b97ee3782314ae5806688e06712938c673
cartoon-raccoon <shaughn.chan@gmail.com>
2022-06-18 10:47:05 +1000

    finalized config API

--- iotsuite/config.py
+++ iotsuite/config.py
@@ -1,4 +1,7 @@
 from configparser import ConfigParser
+import logging
+
+logger = logging.getLogger("config")
 
 # Config sections
 CNC = "CNC"
@@ -18,8 +21,32 @@ class Section:
     """
     Wraps a ConfigParser section proxy to provide access to configs
     """
-    def __init__(self, section):
+    def __init__(self, section, _ty, _global=None):
         self._section = section
+        # type of section
+        self._ty = _ty
+        # global config settings for VMs
+        self._global = _global
+
+    def __getitem__(self, item):
+        # try to return the requested item
+        try:
+            return self._section[item]
+        except KeyError:
+            # if we can't find anything, check the global settings
+            if self._global is not None:
+                try:
+                    return self._global[item]
+                except KeyError:
+                    # nothing even in global settings, return None
+                    return None
+            else:
+                # global settings don't exist for this section,
+                # return None
+                return None
+
+    def item(self, item):
+        return self[item]
 
 class Config:
     def __init__(self, file):
@@ -63,13 +90,50 @@ class Config:
         except KeyError:
             return None
 
+    @property
+    def network(self):
+        try:
+            return Section(self.NETWORK, NETWORK)
+        except AttributeError:
+            return None
+    
+    @property
+    def cnc(self):
+        try:
+            return Section(self.CNC, CNC)
+        except AttributeError:
+            return None
+
+    @property
+    def sandbox(self):
+        try:
+            return Section(self.SANDBOX, SANDBOX)
+        except AttributeError:
+            return None
+    
+    def arch(self, arch):
+        try:
+            return Section(self.cp[arch], arch, self.SANDBOX)
+        except KeyError:
+            return None
+
     def parse_config(self):
 
         if not self._validate_config():
             # todo: raise error
+            logger.error("error: invalid config")
             return
 
-        return
+        setattr(self, CNC, Section(self.cp[CNC], CNC))
+        setattr(self, SANDBOX, Section(self.cp[SANDBOX], SANDBOX))
+        setattr(self, NETWORK, Section(self.cp[NETWORK], NETWORK))
+
+        for arch in _ARCHS:
+            try:
+                setattr(self, arch, Section(self.cp[arch], arch, self.SANDBOX))
+            except KeyError:
+                setattr(self, arch, None)
+                continue
 
     # check that all required sections are present
     # architecture-specific sections are optional
