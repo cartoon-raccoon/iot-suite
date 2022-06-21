41f70cc8809f71456ecd6c1c62371dc49b5c277d
cartoon-raccoon <shaughn.chan@gmail.com>
2022-06-22 09:58:18 +1000

    added config for Net

--- iotsuite/net.py
+++ iotsuite/net.py
@@ -75,7 +75,16 @@ class IptablesRule:
         cmd.extend(["-j", self.target])
         cmd.extend(self.target_args)
 
-        subprocess.run(cmd, check=True)
+        invoke.sudo(' '.join(cmd))
+
+class NetConfig:
+    """
+    Class representing the configuration of a Net class.
+    """
+    def __init__(self, bridge, dhcpconf, ipaddr):
+        self.br = bridge
+        self.dhcp = dhcpconf
+        self.ipaddr = ipaddr
 
 class Net:
     """
@@ -86,10 +95,11 @@ class Net:
     manages the DHCP server and its configuration, as well as injecting and
     flushing `iptables` rules.
     """
-    def __init__(self, bridge, dhcpconf, ipaddr):
-        self.bridge = bridge
-        self.dhcpconf = dhcpconf
-        self.ipaddr = ipaddr
+    def __init__(self, config: NetConfig):
+        self.bridge = config.br
+        self.dhcpconf = config.dhcp
+        self.ipaddr = config.ipaddr
+        self.iptables = []
 
     def setup(self, sudo_passwd):
         logger.debug(f"adding bridge {self.bridge}")
@@ -102,27 +112,32 @@ class Net:
         invoke.sudo(f"ip addr add {self.ipaddr}/24 brd + dev {self.bridge}", password=sudo_passwd)
 
         logger.debug(f"starting dhcpd with configuration file {self.dhcpconf}")
-        self.dhcpd = subprocess.Popen(
-            ["dhcpd", "-f", "-cf", self.dhcpconf],
-            stdin=PIPE, stdout=PIPE, stderr=PIPE,
-        )
-
-        time.sleep(1)
-        retcode = self.dhcpd.poll()
-        if retcode is not None:
-            logger.error(f"error: dhcpd exited with error code {retcode}")
+        invoke.sudo(f"dhcpd -cf {self.dhcpconf}", password=sudo_passwd, hide=True)
 
     def teardown(self, sudo_passwd):
         # flush iptables
         # kill dhcpd
         # remove bridge interface
         logger.debug("tearing down net")
+        invoke.sudo("pkill dhcpd", password=sudo_passwd)
+
+        logger.debug("dhcpd killed, deleting devices")
         invoke.sudo(f"ip link set {self.bridge} down", password=sudo_passwd)
         invoke.sudo(f"ip link delete {self.bridge} type bridge", password=sudo_passwd)
-        self.dhcpd.terminate()
-        ret = self.dhcpd.wait()
-        logger.debug(f"dhcpd exited with {ret}")
-        logger.debug(self.dhcpd.stdout.read())
+
+    def generate_dhcpconf(self):
+        """
+        Generates a `dhcpd` configuration file from the IoTSuite config.
+        """
+        pass
+
+    def insert_iptables(self, rule):
+        rule.insert()
+        self.iptables.append(rule)
+
+    def append_iptables(self, rule):
+        rule.append()
+        self.iptables.append(rule)
 
     def flush_iptables(table: Table, sudo_passwd):
         invoke.sudo(f"iptables -t {table.value} -F", password=sudo_passwd)
