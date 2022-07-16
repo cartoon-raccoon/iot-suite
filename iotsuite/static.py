12480720be9d8f9ab72b7f7e8078b18cce535f84
cartoon-raccoon <shaughn.chan@gmail.com>
2022-07-16 17:06:35 +0800

    fleshed out static analyzer functionality

--- iotsuite/static.py
+++ iotsuite/static.py
@@ -4,23 +4,43 @@ from collections import namedtuple
 import logging
 
 from elftools.elf.elffile import ELFFile
+import magic
 import hashlib
+
+from arch import *
+from config import Config
+import utils
 
 logger = logging.getLogger("static")
 
 Strings = namedtuple('Strings', ['offset', 'string'])
 
+StaticResult = namedtuple("StaticResult", [
+    "hash", "ctph", "strings", "arch", "file"
+])
+
+class StaticError(Exception):
+    
+    def __init__(self, msg):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+    def __repr__(self):
+        return f"StaticError('{self.msg}')"
+
 class StaticAnalyzer:
 
-    def __init__(self, sample):
-        self.set_sample(sample)
+    def __init__(self, config: Config):
+        self.config = config
+        self.magic = magic.Magic()
 
     def set_sample(self, sample):
         """
         Sets the sample to run static analysis on.
         """
         self.sample = sample
-        self.strings = []
         self.elf = ELFFile(open(sample, "rb"))
         
         with open(sample, "rb") as f:
@@ -38,7 +58,17 @@ class StaticAnalyzer:
             pass #todo error handling
 
         output = res.stdout.decode("utf-8")
-        self._parse_strings(output)
+        return self._parse_strings(output)
+
+    def run_hash(self):
+        hash = self.config.STATIC["HashType"]
+
+        if hash.lower() == "sha256":
+            return self.sha256()
+        elif hash.lower() == "md5":
+            return self.md5()
+        else:
+            raise StaticError(f"unknown hash type: {hash}")
 
     def sha256(self):
         """
@@ -58,20 +88,57 @@ class StaticAnalyzer:
         # todo: check entropy and search for UPX magic
         # if entropy is high but UPX magic is not present, packing
         # may still be present but UPX magic is stripped
-        pass
+        utils.todo()
+
+    def run_ctph(self):
+        # todo
+        utils.todo()
 
     def get_arch(self):
         return self.elf.get_machine_arch()
 
+    def get_arch_enum(self):
+        arch = self.get_arch()
+        if arch == ARM:
+            return Arch.ARM
+        elif arch == MIPS:
+            if self.elf.little_endian:
+                return Arch.MIPSEL
+            else:
+                return Arch.MIPS
+        elif arch == M68K:
+            return Arch.M68K
+        elif arch == PPC:
+            return Arch.PPC
+        elif arch == I386:
+            return Arch.I386
+        elif arch == AMD64:
+            return Arch.AMD64
+
+    def get_file(self):
+        return self.magic.from_file(self.sample)
+
+    def run(self):
+        return StaticResult(
+            hash=self.run_hash(),
+            ctph=None, #todo
+            strings=self.run_strings(),
+            arch=self.get_arch_enum(),
+            file=self.get_file()
+        )
+
     def _parse_strings(self, output):
+        ret = []
         for s in output.split("\n"):
             if len(s) < 1:
                 continue
 
             res = s.strip().split()
             offset = int(res[0])
-            strs = " ".join(res[0:])
-            self.strings.append(Strings(offset=offset, string=strs))
+            strs = " ".join(res[1:])
+            ret.append(Strings(offset=offset, string=strs))
+
+        return ret
         
     def _check_packing(self):
         pass
