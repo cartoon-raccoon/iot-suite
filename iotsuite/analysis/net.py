42a0db048c958f8f46d974a9024b28441f29afff
cartoon-raccoon <shaughn.chan@gmail.com>
2022-07-23 19:15:11 +0800

    started implementation of packet parsing

--- iotsuite/analysis/net.py
+++ iotsuite/analysis/net.py
@@ -1,10 +1,33 @@
 import dpkt
 import dpkt.ethernet as eth
+import dpkt.ip as ip
+import dpkt.ip6 as ip6
 import re
+import socket
 
-from ..utils import logger as mainlog
+from abc import ABC
 
-logger = mainlog.getChild("analysis-net")
+import iotsuite.utils as utils
+
+logger = utils.logger.getChild("analysis-net")
+
+class Packet(ABC):
+    """
+    Abstract class for creating decoded packet instances.
+    """
+    def __init__(self, ts, src, dst, sport, dport, data):
+        self.ts = ts
+        self.src = src
+        self.dst = dst
+        self.sport = sport
+        self.dport = dport
+        self.data = data
+
+class IP(Packet):
+    pass
+
+class IP6(Packet):
+    pass
 
 class NetResult:
     def __init__(self, packets, dns):
@@ -42,13 +65,27 @@ class NetAnalyzer:
         # todo: once timing has been implemented in analyse.py,
         # make ts useful
         for ts, buf in self.pcap:
-            packets.append((ts, eth.Ethernet(buf)))
+            packets.append((ts, eth.Ethernet(buf).data))
 
         return packets
 
     def collate_ips(self):
         # todo
         pass
+
+    def _parse_packet_data(self, ts, packet):
+        utils.todo()
+        
+        # todo: use python class magic to streamline this
+        if isinstance(packet, ip.IP):
+            return IP(ts,
+                socket.inet_ntop(socket.AF_INET, packet.src),
+                socket.inet_ntop(socket.AF_INET, packet.dst)
+            )
+        elif isinstance(packet, ip6.IP6):
+            i = socket.AF_INET6
+        else:
+            utils.unreachable()
 
     def _parse_dns_output(self, dns):
         res = self.dns_re.findall(dns)
