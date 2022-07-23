a57afbd0a67142fce1da8086f629457494d3da6a
cartoon-raccoon <shaughn.chan@gmail.com>
2022-07-23 19:15:30 +0800

    started analysis and result serialization

--- iotsuite/orchestrator.py
+++ iotsuite/orchestrator.py
@@ -1,5 +1,6 @@
 import traceback
 import sys
+import json
 
 from .config import Config
 from .dynamic import DynamicAnalyzer
@@ -12,7 +13,12 @@ logger = utils.logger.getChild("orchestrator")
 
 class AnalysisResult:
     """
-    A JSON-serializable class that holds the full results of the analysis.
+    A serializable class that holds the full results of the analysis.
+
+    This class be serialized into JSON or pickled using the `pickle` package.
+
+    This allows the results of the sandbox analysis to be consumed by a generic
+    front-end interface that can deserialize JSON or pickle data.
     """
     def __init__(self, **plugin_results):
         for mod, res in plugin_results:
@@ -20,6 +26,26 @@ class AnalysisResult:
 
     def __getitem__(self, item):
         return self.__dict__[item]
+
+    def __setitem__(self, key, item):
+        setattr(self, key, item)
+
+    def add_new_result(self, mod, res):
+        """
+        Add the new results of an analysis module to the Results.
+        """
+        setattr(self, mod, res)
+
+    def export_as_json(self):
+        # todo: fix the jank
+        return json.dumps(self, 
+            default=lambda o: o.__dict__, 
+            sort_keys=True,
+            indent=4
+        )
+
+    def export_as_pickle(self):
+        utils.todo()
 
 class Orchestrator:
     def __init__(self, config: Config, **params):
@@ -57,7 +83,7 @@ class Orchestrator:
             self.staticres = self.run_static(sample)
         
         if command != "static":
-            self.dynres = self.run_dynamic(sample)
+            self.dynamicres = self.run_dynamic(sample)
 
         # todo: run analysis
 
@@ -69,6 +95,8 @@ class Orchestrator:
     def run_dynamic(self, sample):
         self.static.set_sample(sample)
         arch = self.static.get_arch_enum()
+
+        logger.info(f"Sample has architecture {self.static.get_arch()}")
 
         self.dynamic = DynamicAnalyzer(arch, self.config)
         try: # todo: better error handling my god
@@ -86,10 +114,35 @@ class Orchestrator:
         return res
 
     def run_analysis(self):
-        pass
+        final = AnalysisResult()
+
+        if self._was_completed("static"):
+            pass
+
+        if self._was_completed("dynamic"):
+            logger.debug("running dynamic results analysis")
+            # run syscall analysis
+            syscalls = dict()
+
+            # trace is a path, so it will be affected if we change dir
+            # assume trace is in our current working directory
+            for trace in self.dynamicres.syscalls:
+                pid, syscall = self.syscalls.parse_syscalls(trace)
+                syscalls[pid] = syscall
+            
+            final["syscalls"] = syscalls
+
+        return final
+
 
     def run_plugins(self):
-        pass
+        utils.todo()
+
+    def _was_completed(self, ty):
+        """
+        Returns whether the analysis was completed and the results are stored.
+        """
+        return hasattr(self, f"{ty}res")
 
 
 if __name__ == "__main__":
